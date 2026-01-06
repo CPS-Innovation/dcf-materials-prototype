18aba7ea5828ad2ad9508baf3507d73eda181457
UxTom <thomas.h.brown@icloud.com>
2026-01-06 17:03:40 +0000

    Added structure for summary list, text area & button to /cases/disclosure/assess-non-sensitive/item-disclosable

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,11 +1,13 @@
 const caseMaterials = require('./case-materials.json');
 const disclosureStatuses = require('./disclosure-statuses.json');
+const disclosureNonSensitiveRows = require('./disclosure-non-sensitive-rows.json');
 
 module.exports = {
   caseSort: 'Name',
   taskSort: 'Due date',
   // Insert values here
   caseMaterials,
-  disclosureStatuses
+  disclosureStatuses,
+  disclosureNonSensitiveRows
 
 }
