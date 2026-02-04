6e05e07d49b8d23e5c528159a1cb7f28070177e4
UxTom <thomas.h.brown@icloud.com>
2026-02-04 11:11:33 +0000

    Added data/charge-library.json refactored data/case-indictments.json fop future search functionality

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,6 +2,7 @@ const caseMaterials = require('./case-materials.json')
 const disclosureStatuses = require('./disclosure-statuses.json')
 const disclosureNonSensitiveRows = require('./disclosure-non-sensitive-rows.json')
 const countsData = require('./case-indictments.json')
+const countsData = require('./charge-library.json')
 
 module.exports = {
   caseSort: 'Name',
@@ -10,6 +11,7 @@ module.exports = {
   disclosureStatuses,
   disclosureNonSensitiveRows,
   countsData,
+  chargeLibrary,
 
   // ✅ NEW: rows generated when CPS marks Evidence as "unused non-sensitive"
   disclosureAssessedUnusedRows: []
